0ffdb352f550aaf7b6576cc1af328ef222ff0069
manhtv46k55 <hoangmanh.huc@gmail.com>
2023-08-30 00:01:14 +0700

    aadd

---
+++ GetMarc21/GetMarc21/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GetMarc21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.rtbInput = new System.Windows.Forms.RichTextBox();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(713, 131);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(141, 70);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Get";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // rtbInput
+            // 
+            this.rtbInput.Location = new System.Drawing.Point(65, 40);
+            this.rtbInput.Name = "rtbInput";
+            this.rtbInput.Size = new System.Drawing.Size(601, 278);
+            this.rtbInput.TabIndex = 1;
+            this.rtbInput.Text = "";
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Location = new System.Drawing.Point(909, 40);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(601, 278);
+            this.rtbResult.TabIndex = 2;
+            this.rtbResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1613, 355);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.rtbInput);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.RichTextBox rtbInput;
+        private System.Windows.Forms.RichTextBox rtbResult;
+    }
+}
+
---
+++ GetMarc21/GetMarc21/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetMarc21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            //Chuỗi đầu vào dạng: $aH:$bBrandeis University, USA,$c2002$2Rtvskfsdfsffsdf$xOpfkjwefjwef
+            string input = rtbInput.Text;
+            Dictionary<char, string> result = GetValuesFromInput(input);
+            string strResulr = "";
+            foreach (var item in result)
+            {
+                strResulr += $"${item.Key} = {item.Value}" + "\n";
+            }
+            rtbResult.Text = strResulr;
+        }
+
+        /// <summary>
+        /// Hàm lấy giá trị của Marc21. Xác định Subfield và lấy giá trị trong chuỗi.
+        /// </summary>
+        /// <param name="input">Chuỗi đầu vào dạng: $aH:$bBrandeis University, USA,$c2002$2Rtvskfsdfsffsdf$xOpfkjwefjwef</param>
+        /// <returns>Kết quả trả về là Dictionary: Key - Value</returns>
+        /// For Dic ra để lấy Key - Value
+        public static Dictionary<char, string> GetValuesFromInput(string input)
+        {
+            Dictionary<char, string> values = new Dictionary<char, string>();
+            Regex regex = new Regex(@"\$([a-zA-Z0-9])([^$]*)");
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                char key = match.Groups[1].Value[0];
+                string value = match.Groups[2].Value;
+
+                string newValue = $"{value}";
+                values[key] = newValue;
+            }
+
+            return values;
+        }
+    }
+}
